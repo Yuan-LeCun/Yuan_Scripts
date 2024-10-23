25367a453f966e4ac7afdf2074bf5b8da195fc0c
Rongyao Yuan <101176908+Yuan-LeCun@users.noreply.github.com>
2024-10-23 13:36:40 +0800

    Add files via upload
    遍历本文件夹下所有fchk文件, 并输出HOMO LUMO

--- get_holumo.sh
+++ get_holumo.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
-echo "Running:" $1
-Multiwfn_noGUI $1 << EOF > out.txt
+
+for fchk_file in *.fchk; do
+    # Run Multiwfn_noGUI analysis as shown in predElectro.sh
+    echo "Running:" "$fchk_file"
+    if Multiwfn_noGUI "$fchk_file" << EOF > out.txt
 0
 q
 EOF
+    then
+        # Note: Orbital    57 is HOMO, energy:   -0.331902 a.u.   -9.031509 eV
+        #       Orbital    58 is LUMO, energy:   -0.005486 a.u.   -0.149290 eV
+        HOMO=$(grep "Orbital" out.txt | grep "HOMO" | awk '{print $7}')
+        LUMO=$(grep "Orbital" out.txt | grep "LUMO" | awk '{print $6}')
+        echo "HOMO energy: $HOMO"
+        echo "LUMO energy: $LUMO"
+        echo "$HOMO $LUMO" >> holumo.txt
+    else
+        echo "Error processing $fchk_file"
+    fi
 
-#  Note: Orbital    57 is HOMO, energy:   -0.331902 a.u.   -9.031509 eV
-#        Orbital    58 is LUMO, energy:   -0.005486 a.u.   -0.149290 eV
-HOMO=$(grep "Orbital" out.txt | grep "HOMO" | awk '{print $7}')
-LUMO=$(grep "Orbital" out.txt | grep "LUMO" | awk '{print $6}')
-echo "HOMO energy: $HOMO"
-echo "LUMO energy: $LUMO"
-echo $HOMO $LUMO >> holumo.txt
-
-rm -f out.txt
+    rm -f out.txt
+done
